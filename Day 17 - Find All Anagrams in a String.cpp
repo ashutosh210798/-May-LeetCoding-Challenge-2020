273963eb4873e4e1802f42348ee0389b5046974d
Ashutosh Maheshwari <ashutoshmaheshwari210798@gmail.com>
2020-05-17 23:18:48 +0530

    Create Day 17 - Find All Anagrams in a String.cpp

---
+++ Day 17 - Find All Anagrams in a String.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+	int arr[26][20110];
+	vector<int> findAnagrams(string s, string p) {
+		if (p.length() > s.length()) {
+			return {};
+		}
+		map<char, int>m;
+		for (int i = 0; i < p.length(); i++)m[p[i]]++;
+		for (int i = 0; i < s.length(); i++) {
+			arr[s[i] - 'a'][i]++;
+		}
+
+		for (int i = 0; i < 26; i++) {
+			for (int j = 1; j < 20110; j++) {
+				arr[i][j] += arr[i][j - 1];
+			}
+		}
+		vector<int>ans;
+
+		for (int i = 0; i < s.length() - p.length() + 1; i++) {
+			bool ok = true;
+			for (int j = 0; j < 26; j++) {
+				if (!i) {
+					if (arr[j][p.length() - 1] != m[j + 'a']) {
+						ok = false;
+						break;
+					}
+				}
+				else {
+					if (arr[j][i + p.length() - 1] - arr[j][i - 1] != m[j + 'a']) {
+						ok = false;
+						break;
+					}
+				}
+			}
+			if (ok) {
+				ans.push_back(i);
+			}
+
+		}
+		return ans;
+	}
+};
